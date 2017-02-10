26087c8254f625dba16ff4beb82392a4b76b61f3
orgafni <orgafni93@gmail.com>
2017-02-10 19:22:57 +0200

    add firebase Database to the application

--- app/src/main/java/com/brokenpicinc/brokenpic/model/ModelFirebase.java
+++ app/src/main/java/com/brokenpicinc/brokenpic/model/ModelFirebase.java
@@ -1,8 +1,11 @@
 package com.brokenpicinc.brokenpic.model;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by orgaf_000 on 2/4/2017.
  */
 
 public class ModelFirebase {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 }
