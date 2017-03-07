ac6b8ee39860c2bb0cad5b83449940bbace3c68e
orgafni <orgafni93@gmail.com>
2017-03-07 19:16:18 +0200

    add menu button. only for debugging

--- app/src/main/java/com/brokenpicinc/brokenpic/MainActivity.java
+++ app/src/main/java/com/brokenpicinc/brokenpic/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mvc.imagepicker.ImagePicker;
 
@@ -20,5 +22,17 @@ public class MainActivity extends Activity {
         FragmentTransaction ftr = getFragmentManager().beginTransaction();
         ftr.add(R.id.mainContainer,loginFragment);
         ftr.commit();
+
+
+        Button menuBtn = (Button) findViewById(R.id.menuBth);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final MenuFragment menuFragment = new MenuFragment();
+                FragmentTransaction ftr = getFragmentManager().beginTransaction();
+                ftr.add(R.id.mainContainer,menuFragment);
+                ftr.commit();
+            }
+        });
     }
 }
